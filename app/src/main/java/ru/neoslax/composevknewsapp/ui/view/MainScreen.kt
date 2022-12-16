d04c5d87a5ae2a706bbd74d774705682d5e253d8
Pavel Tretyakov <mail@pstretyakov.ru>
2022-12-16 14:22:34 +0300

    - fix navigation

--- app/src/main/java/ru/neoslax/composevknewsapp/ui/view/MainScreen.kt
+++ app/src/main/java/ru/neoslax/composevknewsapp/ui/view/MainScreen.kt
@@ -1,13 +1,22 @@
 package ru.neoslax.composevknewsapp.ui.view
 
 import androidx.compose.foundation.clickable
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ru.neoslax.composevknewsapp.navigation.AppNavGraph
+import ru.neoslax.composevknewsapp.navigation.Screen
 import ru.neoslax.composevknewsapp.ui.HomeScreen
 import ru.neoslax.composevknewsapp.ui.MainViewModel
 import ru.neoslax.composevknewsapp.ui.data.NavigationItems
@@ -32,7 +41,13 @@ fun MainScreen(viewModel: MainViewModel) {
                         modifier = Modifier.weight(1f),
                         selected = item.screen.route == currentRoute,
                         onClick = {
-                                  navHostController.navigate(item.screen.route)
+                            navHostController.navigate(item.screen.route) {
+                                popUpTo(Screen.NewsFeed.route) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
                         },
                         icon = {
                             Icon(imageVector = item.icon, contentDescription = null)
@@ -55,7 +70,7 @@ fun MainScreen(viewModel: MainViewModel) {
 
 @Composable
 private fun Counter(text: String) {
-    var counter by remember {
+    var counter by rememberSaveable {
         mutableStateOf(0)
     }
     Text(text = "$text: $counter", modifier = Modifier.clickable {
