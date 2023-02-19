cc678f3f700e0a7a7364eaa3e4d05c7b97e04db3
Pavel Tretyakov <mail@pstretyakov.ru>
2023-02-19 12:31:49 +0300

    - add coil

--- app/src/main/java/ru/neoslax/composevknewsapp/MainActivity.kt
+++ app/src/main/java/ru/neoslax/composevknewsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ru.neoslax.composevknewsapp.ui.theme.ComposeVkNewsAppTheme
+import ru.neoslax.composevknewsapp.ui.view.LoadImageScreen
 import ru.neoslax.composevknewsapp.ui.view.MainScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeVkNewsAppTheme {
-                MainScreen()
+//                MainScreen()
+                LoadImageScreen()
             }
         }
     }
---
+++ app/src/main/java/ru/neoslax/composevknewsapp/ui/view/LoadImegeScreen.kt
@@ -0,0 +1,49 @@
+package ru.neoslax.composevknewsapp.ui.view
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun LoadImageScreen() {
+
+    var imageState: Uri? by remember {
+        mutableStateOf(null)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {
+            imageState = it
+        })
+
+    Column {
+
+        AsyncImage(
+            modifier = Modifier.weight(1f),
+            model = imageState, contentDescription = null
+        )
+
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp), onClick = {
+            launcher.launch("image/*")
+        }) {
+            Text(text = "Load Image")
+        }
+    }
+}
