ca16e457b6def8ad0c14dde2266b792110c64f5b
Pavel Tretyakov <mail@pstretyakov.ru>
2023-02-25 19:47:25 +0300

    Revert "add vk sdk"
    This reverts commit f263544a7aee2b555e4de68c09943a2b80284951.

--- app/src/main/java/ru/neoslax/composevknewsapp/MainActivity.kt
+++ app/src/main/java/ru/neoslax/composevknewsapp/MainActivity.kt
@@ -1,9 +1,7 @@
 package ru.neoslax.composevknewsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +12,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.vk.api.sdk.VK
-import com.vk.api.sdk.auth.VKAuthenticationResult.Failed
-import com.vk.api.sdk.auth.VKAuthenticationResult.Success
-import com.vk.api.sdk.auth.VKScope.WALL
 import ru.neoslax.composevknewsapp.ui.MainViewModel
 import ru.neoslax.composevknewsapp.ui.theme.ComposeVkNewsAppTheme
 import ru.neoslax.composevknewsapp.ui.view.MainScreen
@@ -30,20 +24,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeVkNewsAppTheme {
-
-                val launcher = rememberLauncherForActivityResult(contract = VK.getVKAuthActivityResultContract()) {
-                    when(it) {
-                        is Failed  -> {
-
-                        }
-                        is Success -> {
-                            Log.d("TAG","Success")
-                        }
-                    }
-                }
-
-                launcher.launch(listOf(WALL))
-
                 MainScreen(viewModel)
             }
             LazyColumn {}
